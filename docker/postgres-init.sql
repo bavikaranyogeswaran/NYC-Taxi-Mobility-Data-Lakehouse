0aef5735d65ea205ed2fc30eafdfe11c143419a8
Bavikaran Yogeswaran <bavikaranyogeswaran@gmail.com>
2026-06-09 17:48:54 +0530

    secrets are protected

--- docker/postgres-init.sql
+++
@@ -1,8 +0,0 @@
--- Initialize databases and users for Airflow and the lakehouse serving layer
-CREATE USER airflow WITH PASSWORD 'airflow';
-CREATE DATABASE airflow;
-GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
-
-CREATE USER lakehouse WITH PASSWORD 'lakehouse';
-CREATE DATABASE lakehouse;
-GRANT ALL PRIVILEGES ON DATABASE lakehouse TO lakehouse;
